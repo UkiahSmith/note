af905576e4ddc9686406dae497b3f4cc2b60d78b
Ukiah Smith <ukiah@faction42.com>
2018-11-24 12:43:24 -0500

    feat(*) Flags, custom templates
    - feat, All data values can be passed as flags
    - feat, An external template can be used, pass the filename as a flag
    - improvement, Use a featureful slug library to support unicode

--- cmd/note/main.go
+++ cmd/note/main.go
@@ -9,32 +9,90 @@ import (
 	"time"
 
 	"github.com/UkiahSmith/note"
+	"github.com/gosimple/slug"
+	flag "github.com/spf13/pflag"
 )
 
 type NoteData struct {
-	Date    time.Time
-	Title   string
-	Content string
+	Date      time.Time
+	Title     string
+	TitleSlug string
+	Content   string
 }
 
 func main() {
 	var noteD NoteData
+	var fset flag.FlagSet
 	var err error
-	noteD.Date = time.Now()
-	noteD.Title = strings.TrimSpace(strings.Join(os.Args[1:], " "))
+
+	fset.Usage = func() {
+		fmt.Println(`
+Note is a templating tool for note taking.
+
+Usage:
+	note [options] <Title of note>
+
+Options:
+`)
+		fset.PrintDefaults()
+		fmt.Println(`
+Note:
+	The title flag _or_ title arguments is required.
+
+	The date flag must be in one of these formats
+		2006-01-02T15:04:05Z07:00
+		2006-01-02 15:04:05
+		2006-01-02
+`)
+	}
+
+	var templateFile *string = fset.StringP("template", "t", "", "The file to use as a template")
+	fset.StringVar(&noteD.Title, "title", "", "Use this to pre-populate the title variable in a template.")
+	fset.StringVar(&noteD.TitleSlug, "slug", "", "Use this to pre-populate the slug variable.")
+	fset.StringVar(&noteD.Content, "content", "", "Use this to pre-populate the content variable in a template.")
+	var tempDate *string = fset.String("date", "", "Use this to pre-populate the date variable in a template.")
+
+	fset.Parse(os.Args[1:])
 
 	if noteD.Title == "" {
-		fmt.Println("Title is required")
-		usage()
+		noteD.Title = strings.TrimSpace(strings.Join(fset.Args(), " "))
+	}
+
+	if noteD.Title == "" {
+		fset.Usage()
+		fmt.Println("\nerror: Title is required")
 		os.Exit(1)
 	}
 
-	fname := strings.ToLower(strings.Join(os.Args[1:], "-")) + ".md"
+	if noteD.TitleSlug == "" {
+		noteD.TitleSlug = slug.Make(noteD.Title)
+	} else {
+		noteD.TitleSlug = slug.Make(noteD.TitleSlug)
+	}
+
+	{
+		noteD.Date = time.Now()
+		if *tempDate != "" {
+			var t time.Time
+			t, err = time.Parse("2006-01-02T15:04:05Z07:00", *tempDate)
+			if err != nil {
+				t, err = time.Parse("2006-01-02 15:04:05", *tempDate)
+				if err != nil {
+					t, err = time.Parse("2006-01-02", *tempDate)
+				}
+			}
+			if !t.IsZero() {
+				noteD.Date = t
+			}
+		}
+	}
+
+	fname := noteD.TitleSlug + ".md"
 
 	ed := os.Getenv("EDITOR")
 	if ed == "" {
+		fset.Usage()
 		fmt.Println("error: $EDITOR not set.")
-		usage()
 		os.Exit(1)
 	}
 
@@ -44,13 +102,13 @@ func main() {
 	case *os.PathError:
 		writer, err = os.Create(fname)
 		if err != nil {
+			fset.Usage()
 			fmt.Println("error: ", err)
-			usage()
 			os.Exit(1)
 		}
 	case error:
+		fset.Usage()
 		fmt.Println("error: ", err)
-		usage()
 		os.Exit(1)
 	default:
 		note.RunEditor(ed, fname)
@@ -59,30 +117,25 @@ func main() {
 
 	defer writer.Close()
 
-	t, err := template.New("basic").Funcs(note.Tfuncs).Parse(note.BasicTmpl)
+	var tmpl *template.Template
+	if *templateFile == "" {
+		tmpl, err = template.New("note").Funcs(note.Tfuncs).Parse(note.BasicTmpl)
+	} else {
+		tmpl, err = template.New(*templateFile).Funcs(note.Tfuncs).ParseFiles(*templateFile)
+	}
 	if err != nil {
+		fset.Usage()
 		fmt.Println("error: ", err)
-		usage()
 		os.Exit(1)
 	}
 
-	err = t.Execute(writer, noteD)
+	err = tmpl.Execute(writer, noteD)
 	if err != nil {
+		fset.Usage()
 		fmt.Println("error: ", err)
-		usage()
 		os.Exit(1)
 	}
 
 	note.RunEditor(ed, fname)
-	fmt.Println(fname)
-}
-
-func usage() {
-	var usage string = `
-Note is a templating tool for note taking.
-
-Usage:
-	note <Title of note>
-`
-	fmt.Println(usage)
+	fmt.Println("Created file: ", fname)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/UkiahSmith/note
+
+require (
+	github.com/gosimple/slug v1.3.0
+	github.com/rainycape/unidecode v0.0.0-20150907023854-cb7f23ec59be // indirect
+	github.com/spf13/pflag v1.0.3
+)
